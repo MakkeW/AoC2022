df009cd8a47950333bb5b3ff76dbfa85ecbd3ab8
Makke94 <marwe497@student.liu.se>
2022-12-01 07:04:44 +0100

    solve day 1

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -16,15 +16,30 @@ def readInputFile(filename, toInt = False):
 def part1(input):
     #part 1
     print('PART 1')
+    calories_per_elf = []
+    sum = 0
+    for line in input:
+        if line == '':
+            calories_per_elf.append(sum)
+            sum = 0
+        else:
+            sum += int(line)
+
+    print(max(calories_per_elf))
+    return calories_per_elf
 
 
 def part2(input):
+    calories_per_elf = part1(input)
     #part 2
     print('PART 2')
+    calories_per_elf = sorted(calories_per_elf)
+    print(calories_per_elf[-3:])
+    print(sum(calories_per_elf[-3:]))
 
 
 
 #read the input file
 input = readInputFile("input.txt")
 
-part1(input)
+part2(input)
