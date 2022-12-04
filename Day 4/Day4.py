430fe13c5a72f2d578a9898c27bdb818cd48b042
Makke94 <marwe497@student.liu.se>
2022-12-04 20:53:25 +0100

    Day 4 done

--- Day 4/Day4.py
+++ Day 4/Day4.py
@@ -16,15 +16,36 @@ def readInputFile(filename, toInt = False):
 def part1(input):
     #part 1
     print('PART 1')
+    overlaps = 0
+    for line in input:
+        #get the list 
+        result = '-'.join(line.split(',')).split('-')
+        #get the list as numbers and separate variables
+        start1, end1, start2, end2 = [int(x) for x in result]
+        #check if range one contains range two or range two contains range one
+        if (start1 <= start2 and end1 >= end2) or (start2 <= start1 and end2 >= end1):
+            overlaps += 1
 
+    print(f'Overlaps: {overlaps}')
 
 def part2(input):
     #part 2
     print('PART 2')
-
+    overlaps = 0
+    for line in input:
+        #get the list 
+        result = '-'.join(line.split(',')).split('-')
+        #get the list as numbers and separate variables
+        start1, end1, start2, end2 = [int(x) for x in result]
+        #check if range one or range two overlaps
+        if (start1 <= start2 and end1 >= start2) or (start2 <= start1 and end2 >= start1):
+            overlaps += 1
+        
+    print(f'Overlaps: {overlaps}')
 
 
 #read the input file
 input = readInputFile("input.txt")
 
 part1(input)
+part2(input)
